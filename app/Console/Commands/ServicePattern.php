8253d1023c08d554186b5139a25c0dd3d9e87734
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2024-12-23 22:17:45 +0530

    Service with Interface file done

--- app/Console/Commands/ServicePattern.php
+++ app/Console/Commands/ServicePattern.php
@@ -40,25 +40,17 @@ class ServicePattern extends Command
     {
 
         $folderPrefixName = 'Services';
+        $folderInterfacePrefix = 'Interfaces';
         // Take input from artisan command
         $path  = $this->argument('path');
         $pathArray = explode('/', $path);
 
 
-        // Custom Stub will be used to generate this file
-        $stubServicePath = resource_path('stubs/service.stub');
-        $stubInterfacePath = resource_path('stubs/serviceInterface.stub');
+        // $absolute_path = realpath("app/Providers/ServicesServiceProvider.php");
 
+        // dd($absolute_path);
 
-
-        // Stub content is being loaded and modified;
-        $sContent = file_get_contents($stubServicePath);
-        $serviceContent = str_replace(
-            ['{{className}}', '{{classInterface}}'],
-            [$this->fileName, 'I' . $this->fileName],
-            $sContent
-        );
-
+        // return;
 
 
 
@@ -72,19 +64,56 @@ class ServicePattern extends Command
             $filterFolder = array_filter($folder);
 
             $this->fileName = array_pop($filterFolder);
-            dd($this->fileName);
-            return;
             $this->folders = implode('/', $filterFolder);
         } else {
             $this->fileName = $path;
         }
 
 
+        // Namespace
+        $namespace = 'App\\' . $folderPrefixName . ($this->folders ? '\\' . str_replace('/', '\\', $this->folders) : '');
+        $interfaceNamespace = 'App\\' . "$folderPrefixName\Interfaces" . ($this->folders ? '\\' . str_replace('/', '\\', $this->folders) : '');
+        $interfacePath = 'App\\' . "$folderPrefixName\Interfaces" . ($this->folders ? '\\' . str_replace('/', '\\', "$this->folders\I$this->fileName") : "\I$this->fileName");
+
+        // Custom Stub will be used to generate this file
+        $stubServicePath = resource_path('stubs/service.stub');
+        $stubInterfacePath = resource_path('stubs/serviceInterface.stub');
+
+
+
+
+
+        // Stub content is being loaded and modified;
+        $sContent = file_get_contents($stubServicePath);
+        $serviceContent = str_replace(
+            ['{{ namespace }}', '{{ classInterfacePath }}', '{{ className }}', '{{ classInterface }}'],
+            [$namespace, $interfacePath, $this->fileName, 'I' . $this->fileName],
+            $sContent
+        );
+
+        $sInterfaceContent = file_get_contents($stubInterfacePath);
+        $serviceInterfaceContent = str_replace(
+            ['{{ namespace }}', '{{ serviceInterface }}'],
+            [$interfaceNamespace, 'I' . $this->fileName],
+            $sInterfaceContent
+        );
+
+
+
         $folderPath = base_path("app/$folderPrefixName" . ($this->folders ? "/$this->folders" : ''));
+
 
         if (!File::exists($folderPath)) {
             File::makeDirectory($folderPath, 0755, true);
             $this->info("Services '[$folderPath]' created successfully");
+        }
+
+        $interfaceFolderPath = base_path("app/$folderPrefixName/$folderInterfacePrefix" . ($this->folders ? "/$this->folders" : ''));
+
+
+        if (!File::exists($interfaceFolderPath)) {
+            File::makeDirectory($interfaceFolderPath, 0755, true);
+            $this->info("Service Interface '[$interfaceFolderPath]' created successfully");
         }
 
 
@@ -92,10 +121,26 @@ class ServicePattern extends Command
 
         //File path if file not created
         if (!File::exists($filePath)) {
-            File::put($filePath, "<?php \n\n// This is the $this->fileName file.\n");
+            // file_put_contents($filePath, $serviceContent);
+            File::put($filePath, $serviceContent);
             $this->info("File '$this->fileName' created successfully in '$folderPrefixName'.");
         } else {
             $this->warn("File '$this->fileName' already exists in '$folderPrefixName'.");
+        }
+
+
+
+        // Service Interface File Path
+        $interfaceFilePath = $interfaceFolderPath . DIRECTORY_SEPARATOR . "I$this->fileName" . '.php';
+
+
+
+        // Iterface File Path
+        if (!File::exists($interfaceFilePath)) {
+            File::put($interfaceFilePath, $serviceInterfaceContent);
+            $this->info("File [$this->fileName] Interface created successfully in '$folderInterfacePrefix'.");
+        } else {
+            $this->warn("File '$this->fileName' already exists in '$folderInterfacePrefix'.");
         }
 
         return Command::SUCCESS;
